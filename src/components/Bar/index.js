3d7c5a7e6cab57db8122368b3bc641d7d6e2a002
Enes Seval <enesseval@outlook.com>
2023-03-05 15:53:31 +0300

    item buy, sull buttons and moneySlice reducers

--- src/components/Bar/index.js
+++ src/components/Bar/index.js
@@ -4,10 +4,11 @@ import "./style.css";
 
 function Bar() {
 	const money = useSelector((state) => state.money.totalMoney);
+	const firstMoney = useSelector((state) => state.money.firstMoney);
 	return (
 		<div className="bar">
 			<p>Remaining: {money}$ </p>
-			<p> You haven't spent a single dollar! start buying!</p>
+			<p>{money === firstMoney ? "You haven't spent a single dollar! start buying!" : `You only spent ${(1 - money / firstMoney).toFixed(6)}% of the total!`}</p>
 		</div>
 	);
 }
--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -1,32 +1,49 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrementPiece, incrementPiece } from "../../redux/moneySlice";
 import "./style.css";
 
 function Items() {
 	const items = useSelector((state) => state.money.items);
+	const totalMoney = useSelector((state) => state.money.totalMoney);
+	const dispatch = useDispatch();
 
-	const handleSell = (e) => {
-		console.log(e.target.value);
+	const handleSell = (index) => {
+		const value = document.getElementById(`piece${index}`).innerHTML;
+		document.getElementById(`piece${index}`).innerHTML = parseInt(value) - 1;
+		dispatch(
+			decrementPiece({
+				value: parseInt(value) - 1,
+				index: index,
+			}),
+		);
 	};
 
-	const handleBuy = (e) => {
-		console.log(e.target.value);
+	const handleBuy = (index) => {
+		const value = document.getElementById(`piece${index}`).innerHTML;
+		document.getElementById(`piece${index}`).innerHTML = parseInt(value) + 1;
+		dispatch(
+			incrementPiece({
+				value: parseInt(value) + 1,
+				index: index,
+			}),
+		);
 	};
 
 	return (
 		<div className="items">
-			{items.map((item) => (
-				<div key={item.id} className="item_card">
+			{items.map((item, index) => (
+				<div key={index} className="item_card">
 					<img className="item_image" src={item.img} alt="" />
 					<div className="item_footer">
 						<h3>{item.name}</h3>
 						<h4>{item.price} $</h4>
 						<div className="buttons">
-							<button value={item.id} onClick={(e) => handleSell(e)} className="sell">
+							<button onClick={() => handleSell(index)} className="sell" disabled={item.piece === 0 ? true : false}>
 								Sell
 							</button>
-							<span>{item.piece}</span>
-							<button value={item.id} onClick={(e) => handleBuy(e)} className="buy">
+							<span id={`piece${index}`}>{item.piece}</span>
+							<button onClick={() => handleBuy(index)} className="buy" disabled={totalMoney < item.price ? true : false}>
 								Buy
 							</button>
 						</div>
